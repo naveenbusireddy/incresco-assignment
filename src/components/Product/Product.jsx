f91474eed5867617d12ac02099b9a2cb8e37033c
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2022-04-21 02:47:41 +0530

    Product CSS updated

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -5,8 +5,11 @@ const Product = (props) => {
     return (
     
         <li className="product">
-            {props.productData.images.length && <img src={props.productData.images[0].src}/>  }
-            {"Price:"+props.productData.price+", Name:"+props.productData.productName}
+            {props.productData.images.length && <img src={props.productData.images[0].src}/>}
+            <li className="li-product">{props.productData.brand}</li>
+            <li className="li-productName">{props.productData.productName}</li>
+            <li><span className="li-price">Rs.{+props.productData.price}</span> <span className="li-mrp">Rs.{+props.productData.mrp}</span> <span>{props.productData.discountDisplayLabel}</span> </li>
+            
         </li>
 
     )
