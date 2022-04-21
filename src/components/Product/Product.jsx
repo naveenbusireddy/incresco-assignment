795c0ab8b788abaea8fed8452c772f013adb31b5
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2022-04-21 09:42:47 +0530

    final assignment completion

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -5,10 +5,21 @@ const Product = (props) => {
     return (
     
         <li className="product">
-            {props.productData.images.length && <img src={props.productData.images[0].src}/>}
-            <li className="li-product">{props.productData.brand}</li>
-            <li className="li-productName">{props.productData.productName}</li>
-            <li><span className="li-price">Rs.{+props.productData.price}</span> <span className="li-mrp">Rs.{+props.productData.mrp}</span> <span>{props.productData.discountDisplayLabel}</span> </li>
+            <div className="img">
+                {props.productData.images.length && <img src={props.productData.images[0].src} alt={props.productData.productName} />}
+                <div className="img-bottom-left">
+                   {(props.productData.rating) > 0 && <span> {(props.productData.rating).toFixed(1)}★|</span>}
+                   {(props.productData.ratingCount) > 0 && <span> {(props.productData.ratingCount/1000).toFixed(1)}K</span>}                    
+                </div>
+            </div>            
+            
+            <div className="li-product">{props.productData.brand}</div>
+            <div className="li-productName">{props.productData.productName}</div>
+            <div>
+                <span className="li-price">Rs.{+props.productData.price}</span>  
+                <span className="li-mrp">Rs.{+props.productData.mrp}</span>  
+                <span className="li-discount">{props.productData.discountDisplayLabel}</span>
+            </div>
             
         </li>
 
